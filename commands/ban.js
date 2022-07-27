f3d353dd7e71d45daf9a632dd43ec1ed50d82edf
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-07-27 14:50:58 -0300

    refatorado o comando ban

--- commands/ban.js
+++ commands/ban.js
@@ -1,46 +1,46 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
-    name: 'ban',
-    description: 'BAN 4 EVER',
-    async execute(message, args) {
-        if (!message.mentions.members.size > 0) {
-            message.reply('Cê marca a pessoa, pra eu encontrar ela?');
-            return;
-        }
-        else if (message.mentions.members.size > 1) {
-            message.reply('Calma, calma. Um de cada vez, tá?');
-            return;
-        }
+  name: 'ban',
+  description: 'BAN 4 EVER',
+  async execute(message, args) {
+    if (!message.member.roles.cache.has('684013101787512883')) {
+      message.reply('Hmmm... vi aqui nos meus controles que você não tem esse poder, moço. <:su_panda2:697163015891583076>');
+      return;
+    }
 
-        const membro = message.mentions.users.first();
+    if (message.mentions.members.size !== 1) {
+      message.reply('Opa, gostei da sua intenção, gosto de dar martelada... MAS EM QUEM? A sintaxe tem que ser: !ban @infeliz "razão"');
+      return;
+    }
 
-        if (!message.member.roles.cache.has('684013101787512883')) {
-            message.reply('Hmmm... vi aqui nos meus controles que você não tem esse poder, moço. <:su_panda2:697163015891583076>');
-            return;
-        }
-        else {
-            if (!args[0]) {
-                message.reply('Opa, gostei da sua intenção, gosto de dar martelada... MAS EM QUEM?');
-                return;
-            }
+    const membro = message.mentions.users.first();
+    const razao = args.slice(1).join(' ');
 
-            if (membro) {
-                const membroAlvo = message.guild.members.cache.get(membro.id);
+    try {
+      const membroAlvo = message.guild.members.cache.get(membro.id);
+      if (membroAlvo.roles.cache.has('684013101787512883')) {
+        message.reply('Infelizmente esse indivíduo é mais poderoso que eu e não posso executar o comando. <:su_retarded:683866460191457320>');
+        return;
+      }
+      message.delete();
 
-                // Checa se o membro é Consulheiro
-                if (membroAlvo.roles.cache.has('684013101787512883')) {
-                    message.reply('Infelizmente esse indivíduo é mais poderoso que eu e não posso executar o comando. <:su_retarded:683866460191457320>');
-                    return;
-                }
+      const banMsg = new MessageEmbed()
+        .setColor([45, 25, 52])
+        .setTitle('LÁAAA VAAAAAAAAAAI O MARTELO!')
+        .setDescription(`SIIIM! Seu pedido é uma ordem! O membro ${membroAlvo} foi banido com suuuuuuuucesso!`)
+        .setImage('https://c.tenor.com/QHkT9KEz0pYAAAAd/boom-hammer-smash.gif')
+        .addFields({
+          name: 'Razão', value: razao,
+        });
 
-                    membroAlvo.ban();
-                    message.delete();
-                    message.channel.send('Sim, sim, claro! Seu pedido é uma ordem. LÁAAA VAAAAAAAAAAI O MARTELO!');
-                    message.channel.send('https://c.tenor.com/QHkT9KEz0pYAAAAd/boom-hammer-smash.gif');
-                    message.channel.send(`O ${membro} foi banido com sucesso!`);
-                }
-                else {
-                    message.reply('Monsieur, não consegui encontrar esse usuário... me perdoa? Tenta de novo, não me demite <:su_pera:730223124557332540>');
-                }
-            }
-        }
-    };
+      message.channel.send({ embeds: [banMsg] })
+        .then(() => {
+          membroAlvo.ban();
+        });
+    }
+    catch (error) {
+      message.reply('Monsieur, não consegui encontrar esse usuário... me perdoa? Tenta de novo, não me demite <:su_pera:730223124557332540>');
+    }
+  }
+};

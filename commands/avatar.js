9191afa59157749f683c543a909aaee62a48353f
Guilherme Pinho <100877596+guillepinho@users.noreply.github.com>
2022-07-27 14:26:54 -0300

    refatorado comando avatar

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,22 +1,17 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'avatar',
-    description: 'mostra o avatar do migo',
-    execute(client, message, args) {
-        const alvo = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+  name: 'avatar',
+  description: 'mostra o avatar do migo',
+  execute(client, message, args) {
+    const alvo = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
+    const avatar = alvo.avatarURL({ dynamic: true, format: 'png', size: 1024 });
+    
+    const send = new MessageEmbed()
+      .setColor([45, 25, 52])
+      .setTitle(`:camera_with_flash: ${alvo.username}`)
+      .setImage(avatar);
 
-        if (typeof alvo === 'undefined') {
-            message.reply('Sintaxe errada. Use: "!avatar @Usuario", "!avatar IDdoUsuario" ou só "!avatar".');
-            return;
-        }
-
-        const avatar = alvo.avatarURL({ dynamic: true, format: 'png', size: 1024 });
-        const send = new MessageEmbed()
-            .setColor([45, 25, 52])
-            .setTitle(`:camera_with_flash: ${alvo.username}`)
-            .setImage(avatar);
-
-        message.channel.send({ embeds: [send] });
-    }
+    message.channel.send({ embeds: [send] });
+  }
 };

3dd51d065f997b5e4ae4055c9a692a7f9f5f3da7
Guille Pinho <guilherme.apinho@hotmail.com>
2023-05-06 15:39:51 -0300

    refactor(events): finish refactoring events

--- events/general/badWords.js
+++ events/general/badWords.js
@@ -7,5 +7,13 @@ module.exports = {
     if (message.author.bot) return null;
 
     const badWordsList = Object.values(badWords);
+    const doesItMatch = badWordsList.filter((e) => message.content.toLowerCase().includes(e));
+
+    if (!doesItMatch.length) return null;
+
+    const random = Math.floor(Math.random() * 10);
+
+    if (random >= 4) return null;
+    return message.reply(`Ôoooo ${message.author.username}, é com essa boca podre que cê beija seus peguete? <:su_todezoi:684780888261001324>`);
   },
 };
